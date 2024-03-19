b71474a7ae98f99e69c2a25e44ed997da4673376
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-03-19 13:21:49 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "kelvin";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const firstInitial = currentUser[0];
+      const restOfName = currentUser.slice(1);
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!'
+let shortGreeting = 'Welcome, ' + firstInitial + '!'
