29eb66d782382561e9b836ceb40308d770975d2b
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-03-19 14:50:47 +0300

    Second commit

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const firstInitial = currentUser[0];
       const restOfName = currentUser.slice(1);
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!'
-let shortGreeting = 'Welcome, ' + firstInitial + '!'
+// let shortGreeting = 'Welcome, ' + firstInitial + '!'
+let shortGreeting = `Welcome, ${firstInitial} !`
